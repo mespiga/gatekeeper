0c6917a11bdd796a743ab0e25229bce587e52686
miguelespiga <miguel@coderhat.com>
2018-04-11 00:23:15 -0700

    update

--- spring-boot/src/main/java/app/data/jpa/domain/Room.java
+++ spring-boot/src/main/java/app/data/jpa/domain/Room.java
@@ -30,22 +30,22 @@ public class Room {
     public Long id;
 
     @Getter @Setter public String name;
-    @Getter @Setter public String xMin;
-    @Getter @Setter public String xMax;
-    @Getter @Setter public String yMin;
-    @Getter @Setter public String yMax;
+    @Getter @Setter public String x1Min;
+    @Getter @Setter public String x1Max;
+    @Getter @Setter public String y1Min;
+    @Getter @Setter public String y1Max;
     @Getter @Setter public String sparkRoomId;
-
-    public Room() {}
 
     public Room(String name, String xMin, String yMin, String xMax, String yMax, String sparkRoomId) {
         this.name = name;
-        this.xMin = xMin;
-        this.yMin = yMin;
-        this.xMax = xMax;
-        this.yMin = yMax;
+        this.x1Min = xMin;
+        this.y1Min = yMin;
+        this.x1Max = xMax;
+        this.y1Max = yMax;
         this.sparkRoomId = sparkRoomId;
     }
+
+    public Room() {}
 
 }
 
